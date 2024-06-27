21a0bbb50be966e3aeaa46697b68929f00e27cba
mehran_mujawar <mehranm216@gmail.com>
2024-06-27 18:21:19 +0530

    get and set static variable using extern

---
+++ get_set_static_extern.c
@@ -0,0 +1,42 @@
+// Get set static number and function from a file other than where it is 
+// declared using extern
+
+// FILE1.C
+
+#include <stdio.h>
+
+extern int get_static_number ();
+extern void set_static_number (int value);
+extern void (*fptr)();
+
+int main ()
+{
+	printf ("%d \n", get_static_number());	   // Prints the value of static variable num.
+	set_static_number(100);
+	printf ("%d \n", get_static_number());	
+
+	fptr ();    // Prints "This is a static function."
+	return 0;
+}
+
+// FILE2.C
+
+#include <stdio.h>
+
+static int num = 21;
+
+int get_static_number ()
+{
+	return num;
+}
+
+void set_static_number (int value)
+{
+	num = value;
+}
+
+static void static_function () { printf ("this is a static function\n"); }
+
+void (*fptr)() = static_function;
+
+// GCC FILE1.C FILE2.C
