19788e669d25dfb77e727796634c5179282d2df4
mehran_mujawar <mehranm216@gmail.com>
2024-06-28 15:43:36 +0530

    strcmp & strncmp

---
+++ string_compare.c
@@ -0,0 +1,53 @@
+// STRING COMPARE (strcmp)
+
+#include <stdio.h>
+
+int string_compare (char *str1, char *str2)
+{
+    while (*str1) {
+        
+        if (*str1 != *str2)
+            return (*str1 - *str2);
+            
+        str1++;
+        str2++;
+    }
+}
+
+int main ()
+{
+    char *str1 = "mehran";
+    char *str2 = "mehrab";
+    
+    printf ("%d %d", string_compare (str1, str2), 'n' - 'b');
+}
+
+// STRING COMPARE (strncmp)
+
+#include <stdio.h>
+
+int string_compare (char *str1, char *str2, int limit)
+{
+    int i = 0;
+    while (*str1 && i < limit) {
+        
+        if (*str1 != *str2)
+            return (*str1 - *str2);
+            
+        str1++;
+        str2++;
+        i++;
+    }
+    
+    return 0;
+}
+
+int main ()
+{
+    char *str1 = "mehran";
+    char *str2 = "mehrab";
+    
+    int limit = 6;
+    
+    printf ("%d ", string_compare (str1, str2, limit));
+}
