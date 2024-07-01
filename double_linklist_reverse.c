15b1bf9f05f845afe5d761956b1f8c31200b2804
21mehran <mehranm216@gmail.com>
2024-07-01 20:03:17 +0530

    reverse - data manipulation.

--- double_linklist_reverse.c
+++ double_linklist_reverse.c
@@ -1,4 +1,6 @@
 // Reverse a double linked list.
+// 1. Pointer Manipulation. 
+// 2. Data Manipulation.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -47,6 +49,7 @@ void print_reverse (struct node *head)
     printf ("\n");
 }
 
+// Reverse list using pointer manipulation.
 struct node *actually_reverse (struct node *head) 
 {
     struct node *ptr = head;
@@ -64,6 +67,32 @@ struct node *actually_reverse (struct node *head)
     }
 
     return ptr;
+}
+
+// Reverse list using data manipulation.
+struct node *actually_reverse (struct node *head)
+{
+    struct node *start = head;
+    struct node *end = NULL;
+
+    while (start->next) {
+        start = start->next;
+    }
+
+    end = start; 
+    start = head;
+    
+    while (start && end && start > end)
+    {
+        int temp = start->data;
+        start->data = end->data;
+        end->data = temp;
+
+        start = start->next;
+        end = end->prev;
+    }
+    
+    return head;    
 }
 
 struct node *insert_start (struct node *head, int data)
