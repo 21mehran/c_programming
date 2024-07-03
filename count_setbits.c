4296d97e9e7fd9ae1dc29b6ad283926477c3602d
mehran_mujawar <mehranm216@gmail.com>
2024-07-03 10:58:48 +0530

    count set bits using recursion.

---
+++ count_setbits.c
@@ -0,0 +1,18 @@
+// Count no. of set bits using Recurssion.
+// Brian Kernigans Algorithm.
+
+#include <stdio.h>
+
+int set_bits (int num, int count) 
+{
+    if (num == 0)
+        return count;
+    else 
+        set_bits (num & (num - 1), count + 1);
+}
+
+int main()
+{
+    printf ("%d ", set_bits (15, 0));
+    return 0;
+}
