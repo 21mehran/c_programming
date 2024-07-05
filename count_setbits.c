2bb15f2b08b137573a65d7515aff92b1a57438be
mehran_mujawar <mehranm216@gmail.com>
2024-07-05 15:49:14 +0530

    static variable

--- count_setbits.c
+++ count_setbits.c
@@ -11,6 +11,20 @@ int set_bits (int num, int count)
         set_bits (num & (num - 1), count + 1);
 }
 
+// Using 1 argument and static variable. 
+
+int set_bits (int num)
+{
+    int static count = 0;
+
+    if (!num)
+        return count;
+    else {
+        count++;
+        set_bits (num & (num - 1));
+    }
+}
+
 int main()
 {
     printf ("%d ", set_bits (15, 0));
