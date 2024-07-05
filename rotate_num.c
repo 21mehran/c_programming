d448e8f5f2ecce6c7c6f2b100886d4ebf94a1abe
mehran_mujawar <mehranm216@gmail.com>
2024-07-05 18:01:43 +0530

    rotate the given number.

---
+++ rotate_num.c
@@ -0,0 +1,26 @@
+// Rotate a number using bitwise.
+
+// ex 0000 1010 -> 0101 0000
+// 10 -> 80
+
+#include <stdio.h>
+
+int main () 
+{
+    int num = 1; 
+    int rev = 0;
+    int position = 0;
+    int total_bits = sizeof(num) * 8; 
+
+    while (position < total_bits) {
+        
+        rev = rev | (((num >> position) & 1) << (total_bits - 1 - position));
+        position++;
+    }
+
+    printf("Rotated bits result: %u\n", rev);
+    
+    return 0;
+}
+
+
