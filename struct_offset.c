ef552f1fdf05fb67cdb27527a5970070dc218019
21mehran <mehranm216@gmail.com>
2024-07-07 11:11:34 +0530

    implement struct offset_of.

---
+++ struct_offset.c
@@ -0,0 +1,28 @@
+// Find the offset of struct elements.
+
+#include <stdio.h>
+
+#define OFFSET(a,b) ((char *)(b) - (char *)(a))
+
+struct test {
+  double a;
+  char b;
+  int c;
+  short d;
+};
+
+int struct_offset (struct test *ptr1, void *ptr2)
+{
+    return (char *)ptr2 - (char *)ptr1;
+}
+
+int main ()
+{
+    struct test p1;
+
+    printf ("Offset a : using function - %d\n", struct_offset (&p1, &p1.a));     // Using function call.
+    printf ("Offset b : using function - %d\n", struct_offset (&p1, &p1.b));
+    printf ("Offset c : using MACRO - %d\n", OFFSET (&p1, &p1.c));             // Using MACRO.
+    printf ("Offset d : using MACRO - %d\n", OFFSET (&p1, &p1.d));
+}
+
