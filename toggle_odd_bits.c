031e546a89f8cece2f6da0663d2abbb9fa069514
21mehran <mehranm216@gmail.com>
2024-07-14 19:58:03 +0530

    Toggle even/odd bits.

--- toggle_odd_bits.c
+++ toggle_odd_bits.c
@@ -6,10 +6,13 @@ int main()
 {
    int num = 0b10101010;
    
+   printf ("%d ", (num ^ 0x55));  // Toggle Odd bits.
+   printf ("%d ", (num ^ 0xAA));  // Toggle Even bits.
+
    for (int i = 0; i < 8; i = i + 2)
         num = num ^ (1 << i);
         
-    printf ("%d ", num);    
+   printf ("%d ", num);    
    
    return 0;
 }
