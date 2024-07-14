f3e0c2acf60201ef82becab58df61a359d7ec617
21mehran <mehranm216@gmail.com>
2024-07-14 07:36:15 +0530

    Manacher's Algorithm.

--- longest_palindromic_substring_manachers_algorithm.c
+++ longest_palindromic_substring_manachers_algorithm.c
@@ -1,40 +1,37 @@
-// Find the longest palindromic substring using Manacher's algorithm
-
 #include <stdio.h>
 #include <string.h>
 
-void longest_palindrome (char* str, char* result)
+void longest_palindrome (char *str, char *result) 
 {
     int len = strlen(str);
 
     // Preprocess the string
-    char str_hash [2 * len + 3];
-    memset (str_hash, 0, sizeof (str_hash));
-    
-    int arr [2 * len + 3];
+    char str_hash[2 * len + 3];
+    int arr[2 * len + 3];
+    memset(str_hash, 0, sizeof(str_hash));
+    memset(arr, 0, sizeof(arr));
+
     int center = 0, right = 0;  // center and right edge of the current palindrome
     int i_mirror, maxLen = 0, centerIndex = 0;
-    
-    // Processing the strings.
+
+    // Processing the string
     str_hash[0] = '^';
-    
     for (int i = 0; i < len; i++) {
         str_hash[2 * i + 1] = '#';
-        str_hash[2 * i + 2] = str_hash[i];
+        str_hash[2 * i + 2] = str[i];
     }
-    
     str_hash[2 * len + 1] = '#';
     str_hash[2 * len + 2] = '$';
 
     for (int i = 1; i < 2 * len + 2; i++) {
         i_mirror = 2 * center - i;
 
-        // When right boundary exceeds the i.
-        if (right > i) 
+        // When right boundary exceeds i
+        if (right > i)
             arr[i] = (right - i < arr[i_mirror]) ? (right - i) : arr[i_mirror];
         
         // Comparing elements around the center
-        while (str_hash[i + 1 + arr[i]] == str_hash[i - 1 - arr[i]]) 
+        while (str_hash[i + 1 + arr[i]] == str_hash[i - 1 - arr[i]])
             arr[i]++;
     
         // Move the center    
@@ -56,7 +53,7 @@ void longest_palindrome (char* str, char* result)
 
 int main () 
 {
-    char s[] = "ababcbab";
+    char s[] = "aaaababcbaba";
     char result[20];
     
     longest_palindrome(s, result);
