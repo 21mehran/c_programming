b6ae753c8ecabe341320b052d5a40c1a04718137
mehran <mehranm216@gmail.com>
2024-08-29 12:11:51 +0530

    Selection Sort - Implementation.

--- selection_sort.c
+++ selection_sort.c
@@ -1,36 +1,44 @@
-// SELECTION SORT - ALGORITHM
+// SELECTION SORT - IMPLEMENTATION.
 
 #include <stdio.h>
 
-int selection_sort (int arr[], int len) 
+void print_array (int arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+        printf ("%d ", arr[i]);
+}
+
+void swap (int *one, int *two) 
+{
+    int temp = *one;
+    *one = *two;
+    *two = temp;
+}
+
+void selection_sort (int arr[], int len) 
 {
     for (int i = 0; i < len; i++) {
         
         int min_index = i;
         
-        for (int j = i + 1; j < len; j++) {
-            
-            if (arr[j] < arr[min_index])
+        for (int j = i + 1; j < len; j++) 
+        {
+            if (arr[j] < arr[min_index]) 
                 min_index = j;
         }
-            
-        if (min_index != i) {
-            int temp = arr[i];
-            arr[i] =  arr[min_index];
-            arr[min_index] = temp;
-        }    
+        
+        if (min_index != i)
+        swap (&arr[min_index], &arr[i]);
     }
 }
 
-
 int main ()
 {
-    int arr[] = {2,3,1,5,7,6};
-    int len = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = { 2, 8, 5, 3, 9, 4, 1 };
+    int len = sizeof (arr) / sizeof (arr[0]);
     
     selection_sort (arr, len);
+    print_array (arr, len);
     
-    for (int i = 0; i < 6; i++)
-        printf ("%d ", arr[i]);
+    return 0;
 }
-
